6294d2940f68b08ab63453a9d74314a622c03cae
KyotoDalex <funlightgaming@gmail.com>
2023-07-21 09:02:15 +0600

    1233

--- ugrainfo/settings/dev.py
+++ ugrainfo/settings/dev.py
@@ -7,7 +7,7 @@ DEBUG = False
 SECRET_KEY = "django-insecure-8+(fgey3gfk39v390_%c%_h9tf4cii@as+p8a_9+$lf9uka5w1"
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['ugrainfo.ru-central1.internal','www.ugrainfo.ru-central1.internal.com']
+ALLOWED_HOSTS = ['testugra','www.testugra.com']
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
