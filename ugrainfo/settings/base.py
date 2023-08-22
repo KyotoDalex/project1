c8d3f8ed1dec6964fd8294dc5138f35bad2f8cbd
KyotoDalex <funlightgaming@gmail.com>
2023-08-22 12:35:52 +0600

    changes in post page and in main page carousel

--- ugrainfo/settings/base.py
+++ ugrainfo/settings/base.py
@@ -131,14 +131,11 @@ WAGTAILEMBEDS_FINDERS = [
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": 'manga_db',
-        "USER": 'postgres',
-        "PASSWORD": '123123',
-        "HOST": 'localhost',
-        "PORT": '5432'
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
